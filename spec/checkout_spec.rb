992bfccd216e96f7d330dc8cf57cbc3f116b78f9
Chris Hanks <christopher.m.hanks@gmail.com>
2014-02-14 19:08:21 -0800

    Reorganize specs some more.

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Pond do
+describe Pond, "#checkout" do
   it "should yield objects specified in the block" do
     pond = Pond.new { 1 }
     pond.checkout { |i| i.should == 1 }
@@ -12,7 +12,7 @@ describe Pond do
     value.should == 'value'
   end
 
-  it "should instantiate objects only as needed" do
+  it "should instantiate objects when needed" do
     int  = 0
     pond = Pond.new { int += 1 }
 
@@ -39,12 +39,6 @@ describe Pond do
     pond.size.should == 1
   end
 
-  it "should eagerly instantiate objects if the option is given" do
-    int = 0
-    pond = Pond.new(:eager => true){int += 1}
-    pond.available.should == (1..10).to_a
-  end
-
   it "should not instantiate objects in excess of the specified maximum_size" do
     object = nil
     pond = Pond.new(:maximum_size => 1) { object = Object.new }
@@ -59,7 +53,7 @@ describe Pond do
     object_ids.uniq.should == [object.object_id]
   end
 
-  it "should check out different objects to different threads" do
+  it "should give different objects to different threads" do
     int  = 0
     pond = Pond.new { int += 1 }
 
@@ -142,7 +136,7 @@ describe Pond do
     threads.each &:join
   end
 
-  it "should treat the collection of objects as a queue" do
+  it "should treat the collection of objects as a queue by default" do
     int  = 0
     pond = Pond.new { int += 1 }
     results = []
@@ -174,7 +168,7 @@ describe Pond do
     results.should == (1..4).cycle(4).to_a
   end
 
-  it "should treat the collection of objects as a stack, if configured that way" do
+  it "should treat the collection of objects as a stack if configured that way" do
     int  = 0
     pond = Pond.new(:collection => :stack) { int += 1 }
     results = []
@@ -237,7 +231,7 @@ describe Pond do
     pond.available.should == [1]
   end
 
-  it "should not be blocking if the object instantiation takes a long time" do
+  it "should not block other threads if the object instantiation takes a long time" do
     t = nil
     q1, q2, q3 = Queue.new, Queue.new, Queue.new
     pond = Pond.new do
@@ -266,7 +260,7 @@ describe Pond do
     t.join
   end
 
-  it "should not be left in a bad state if object instantiation fails" do
+  it "should not leave the Pond in a bad state if object instantiation fails" do
     int = 0
     error = false
     pond = Pond.new do
@@ -295,113 +289,5 @@ describe Pond do
     pond.size.should == 1
     pond.allocated.should == {}
     pond.available.should == [1]
-  end
-
-  it "should have its collection type gettable and settable" do
-    pond = Pond.new { Object.new }
-    pond.collection.should == :queue
-    pond.collection = :stack
-    pond.collection.should == :stack
-
-    pond = Pond.new(:collection => :stack) { Object.new }
-    pond.collection.should == :stack
-    pond.collection = :queue
-    pond.collection.should == :queue
-
-    procs = [
-      proc{pond.collection = nil},
-      proc{Pond.new(:collection => nil) { Object.new }},
-      proc{pond.collection = :blah},
-      proc{Pond.new(:collection => :blah) { Object.new }}
-    ]
-
-    procs.each { |p| p.should raise_error RuntimeError, /Bad value for Pond collection:/ }
-  end
-
-  it "should have its timeout gettable and settable" do
-    pond = Pond.new { Object.new }
-    pond.timeout.should == 1
-    pond.timeout = 4
-    pond.timeout.should == 4
-
-    pond = Pond.new(:timeout => 3.7) { Object.new }
-    pond.timeout.should == 3.7
-    pond.timeout = 1.9
-    pond.timeout.should == 1.9
-
-    procs = [
-      proc{pond.timeout = nil},
-      proc{Pond.new(:timeout => nil) { Object.new }},
-      proc{pond.timeout = :blah},
-      proc{Pond.new(:timeout => :blah) { Object.new }}
-    ]
-
-    procs.each { |p| p.should raise_error RuntimeError, /Bad value for Pond timeout:/ }
-  end
-
-  it "should have its maximum_size gettable and settable" do
-    pond = Pond.new { Object.new }
-    pond.maximum_size.should == 10
-    pond.maximum_size = 7
-    pond.maximum_size.should == 7
-    pond.maximum_size = 0
-    pond.maximum_size.should == 0
-    pond.maximum_size = 2
-    pond.maximum_size.should == 2
-
-    procs = [
-      proc{pond.maximum_size = nil},
-      proc{Pond.new(:maximum_size => nil) { Object.new }},
-      proc{pond.maximum_size = :blah},
-      proc{Pond.new(:maximum_size => :blah) { Object.new }},
-      proc{pond.maximum_size = 4.0},
-      proc{Pond.new(:maximum_size => 4.0) { Object.new }},
-    ]
-
-    procs.each { |p| p.should raise_error RuntimeError, /Bad value for Pond maximum_size:/ }
-  end
-
-  it "when the maximum_size is decreased should free available objects" do
-    int  = 0
-    pond = Pond.new(:eager => true) { int += 1 }
-
-    pond.available.should == (1..10).to_a
-    pond.maximum_size = 8
-    pond.available.should == (3..10).to_a
-    pond.maximum_size = 10
-    pond.available.should == (3..10).to_a
-    pond.maximum_size = 9
-    pond.available.should == (3..10).to_a
-  end
-
-  it "when the maximum_size is decreased should free available objects and checked-out objects upon return" do
-    int = 0
-    pond = Pond.new(:eager => true, :maximum_size => 2) { int += 1 }
-    pond.available.should == [1, 2]
-
-    q1, q2 = Queue.new, Queue.new
-    t = Thread.new do
-      pond.checkout do |i|
-        i.should == 1
-        q1.push nil
-        q2.pop
-      end
-    end
-
-    q1.pop
-
-    pond.maximum_size = 0
-    pond.maximum_size.should == 0
-
-    pond.size.should == 1
-    pond.available.should == []
-    pond.allocated.should == {t => 1}
-
-    q2.push nil
-    t.join
-
-    pond.size.should == 0
-    pond.available.should == []
-    pond.allocated.should == {}
   end
 end
---
+++ spec/config_spec.rb
@@ -0,0 +1,117 @@
+require 'spec_helper'
+
+describe Pond, "configuration" do
+  it "should eagerly instantiate objects if the option is given" do
+    int = 0
+    pond = Pond.new(:eager => true){int += 1}
+    pond.available.should == (1..10).to_a
+  end
+
+  it "should have its collection type gettable and settable" do
+    pond = Pond.new { Object.new }
+    pond.collection.should == :queue
+    pond.collection = :stack
+    pond.collection.should == :stack
+
+    pond = Pond.new(:collection => :stack) { Object.new }
+    pond.collection.should == :stack
+    pond.collection = :queue
+    pond.collection.should == :queue
+
+    procs = [
+      proc{pond.collection = nil},
+      proc{Pond.new(:collection => nil) { Object.new }},
+      proc{pond.collection = :blah},
+      proc{Pond.new(:collection => :blah) { Object.new }}
+    ]
+
+    procs.each { |p| p.should raise_error RuntimeError, /Bad value for Pond collection:/ }
+  end
+
+  it "should have its timeout gettable and settable" do
+    pond = Pond.new { Object.new }
+    pond.timeout.should == 1
+    pond.timeout = 4
+    pond.timeout.should == 4
+
+    pond = Pond.new(:timeout => 3.7) { Object.new }
+    pond.timeout.should == 3.7
+    pond.timeout = 1.9
+    pond.timeout.should == 1.9
+
+    procs = [
+      proc{pond.timeout = nil},
+      proc{Pond.new(:timeout => nil) { Object.new }},
+      proc{pond.timeout = :blah},
+      proc{Pond.new(:timeout => :blah) { Object.new }}
+    ]
+
+    procs.each { |p| p.should raise_error RuntimeError, /Bad value for Pond timeout:/ }
+  end
+
+  it "should have its maximum_size gettable and settable" do
+    pond = Pond.new { Object.new }
+    pond.maximum_size.should == 10
+    pond.maximum_size = 7
+    pond.maximum_size.should == 7
+    pond.maximum_size = 0
+    pond.maximum_size.should == 0
+    pond.maximum_size = 2
+    pond.maximum_size.should == 2
+
+    procs = [
+      proc{pond.maximum_size = nil},
+      proc{Pond.new(:maximum_size => nil) { Object.new }},
+      proc{pond.maximum_size = :blah},
+      proc{Pond.new(:maximum_size => :blah) { Object.new }},
+      proc{pond.maximum_size = 4.0},
+      proc{Pond.new(:maximum_size => 4.0) { Object.new }},
+    ]
+
+    procs.each { |p| p.should raise_error RuntimeError, /Bad value for Pond maximum_size:/ }
+  end
+
+  it "when the maximum_size is decreased should free available objects" do
+    int  = 0
+    pond = Pond.new(:eager => true) { int += 1 }
+
+    pond.available.should == (1..10).to_a
+    pond.maximum_size = 8
+    pond.available.should == (3..10).to_a
+    pond.maximum_size = 10
+    pond.available.should == (3..10).to_a
+    pond.maximum_size = 9
+    pond.available.should == (3..10).to_a
+  end
+
+  it "when the maximum_size is decreased should free available objects and checked-out objects upon return" do
+    int = 0
+    pond = Pond.new(:eager => true, :maximum_size => 2) { int += 1 }
+    pond.available.should == [1, 2]
+
+    q1, q2 = Queue.new, Queue.new
+    t = Thread.new do
+      pond.checkout do |i|
+        i.should == 1
+        q1.push nil
+        q2.pop
+      end
+    end
+
+    q1.pop
+
+    pond.maximum_size = 0
+    pond.maximum_size.should == 0
+
+    pond.size.should == 1
+    pond.available.should == []
+    pond.allocated.should == {t => 1}
+
+    q2.push nil
+    t.join
+
+    pond.size.should == 0
+    pond.available.should == []
+    pond.allocated.should == {}
+  end
+end
